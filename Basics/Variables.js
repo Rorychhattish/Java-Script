319bedca750262d7d317a66b221378b82427b4c5
Chhatish kumar Sah <rorychhattish@gmail.com>
2026-03-06 11:42:06 +0000

    Variables

---
+++ Basics/Variables.js
@@ -0,0 +1,16 @@
+const accountID = 4578
+let accountEmail = "Rory@gmail.com"
+var accountPassword = "72364"
+accountCity = "Kathmandu"
+let accountCity;
+
+// accountID = 65 // Not allowed
+
+console.log(accountID);
+console.table([ accountEmail, accountPassword, accountCity])
+
+// This is for single line comment.
+/*
+Prefer not to use var
+because of issue in block scope and functional scope.
+*/
