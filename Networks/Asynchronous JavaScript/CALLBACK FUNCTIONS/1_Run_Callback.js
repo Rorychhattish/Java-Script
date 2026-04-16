a35db6cd5567c219446be70276e6cdc838d7d316
Chhatish kumar Sah <rorychhattish@gmail.com>
2026-04-16 19:21:39 +0545

    feat: implement immediate callback execution

---
+++ Networks/Asynchronous JavaScript/CALLBACK FUNCTIONS/1_Run_Callback.js
@@ -0,0 +1,16 @@
+// Goal: Complete Run Callback
+// Let's try this out for ourselves! Complete the function runCallback to invoke callbackFunction immediately.
+
+
+
+/**
+ * Runs a callback function immediately
+ * @param {function} callbackFunction
+ */
+function runCallback(callbackFunction) {
+    setTimeout(() => {
+        callbackFunction();
+    }, 1000);
+}
+
+module.exports = runCallback;
