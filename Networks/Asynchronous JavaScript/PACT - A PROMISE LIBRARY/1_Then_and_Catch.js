3cc682c265a60718b9f70fa8f39aff400b98ca7e
Chhatish kumar Sah <rorychhattish@gmail.com>
2026-04-24 15:05:31 +0545

    feat: define then and catch methods in Pact class

---
+++ Networks/Asynchronous JavaScript/PACT - A PROMISE LIBRARY/1_Then_and_Catch.js
@@ -0,0 +1,19 @@
+// Goal: Add the Methods
+// Your first task is to create two class methods catch and then. These methods are used by promises to wire up callbacks.
+//  See examples of how these methods will be used in details.
+// To pass the assertions in testPact.js, you'll simply need to create the class methods catch and then on your Pact class. There is no need to implement any functionality just yet!
+
+
+
+class Pact {
+    constructor() {
+        // We'll add logic here later!
+    }
+    then(callback) {
+        // This method will eventually handle resolved values
+    }
+    catch(callback) {
+        // This method will eventually handle errors
+    }
+}
+module.exports = Pact;
